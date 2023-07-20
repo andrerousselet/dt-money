79fcc80155822159b5ad4aa7f36b019a994a1a08
Andre Rousselet <andre098@hotmail.com>
2023-07-20 11:19:18 -0300

    feat(utils): add formatPrice function

---
+++ src/utils/formatPrice.ts
@@ -0,0 +1,7 @@
+export function formatPrice(price: number) {
+  const formattedPrice = Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(price);
+  return formattedPrice;
+}
