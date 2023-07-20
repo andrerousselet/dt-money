02ba2842972c59d566b5e8eadcad502a830f7d1d
Andre Rousselet <andre098@hotmail.com>
2023-07-20 12:25:08 -0300

    refactor(custom-hook): add useSummary hook abstracting calculations from summary component

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,28 +1,10 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
-import { useTransactions } from "../../hooks/useTransactions";
 import { formatPrice } from "../../utils/formatPrice";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
-  const { transactions } = useTransactions();
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  );
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
